678ba265c0e20d9cd486996600e1287bd7eed91c
Qiu Xiang <i@7c00.cc>
2017-01-17 19:58:11 +0800

    add android source code

--- android/src/main/java/com/qiuxiang/rn/RecordingModule.java
+++ android/src/main/java/com/qiuxiang/rn/RecordingModule.java
@@ -1,22 +1,72 @@
-
 package com.qiuxiang.rn;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-public class RecordingModule extends ReactContextBaseJavaModule {
+class RecordingModule extends ReactContextBaseJavaModule {
+    private AudioRecord audioRecord;
+    private boolean isRecording;
+    private int bufferSize = 2048;
 
-  private final ReactApplicationContext reactContext;
+    RecordingModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  public RecordingModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    @Override
+    public String getName() {
+        return "Recording";
+    }
 
-  @Override
-  public String getName() {
-    return "Recording";
-  }
+    @ReactMethod
+    public void start(int sampleRate, int bufferSize) {
+        this.bufferSize = bufferSize;
+        audioRecord = new AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                sampleRate,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufferSize);
+        audioRecord.startRecording();
+        isRecording = true;
+        new Thread(new Runnable() {
+            public void run() {
+                recording();
+            }
+        }, "RecordingThread").start();
+    }
+
+    @ReactMethod
+    public void stop() {
+        if (audioRecord != null) {
+            isRecording = false;
+            audioRecord.stop();
+            audioRecord.release();
+        }
+    }
+
+    private void recording() {
+        byte data[] = new byte[bufferSize];
+        while (isRecording) {
+            WritableArray array = Arguments.createArray();
+            audioRecord.read(data, 0, bufferSize);
+            for (byte value : data) {
+                array.pushInt(value);
+            }
+            sendEvent("recording", array);
+        }
+    }
+
+    private void sendEvent(String eventName, Object params) {
+        getReactApplicationContext()
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
 }
