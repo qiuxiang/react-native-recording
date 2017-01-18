0eb52304c88b92e5316147610ce2e44a81ae07d4
Qiu Xiang <i@7c00.cc>
2017-01-18 13:57:44 +0800

    add sound power indicator

--- example/src/example.js
+++ example/src/example.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react'
 import {
   View,
+  Animated,
   NativeModules,
   NativeAppEventEmitter } from 'react-native'
 
 export default class Main extends Component {
   constructor() {
     super()
-    NativeModules.Recording.start(8000, 2048)
-    NativeAppEventEmitter.addListener('recording', (data) => {
-      console.log(data.length)
-    })
+    this.state = {
+      power: new Animated.Value(0),
+      width: 0,
+    }
+  }
+
+  componentDidMount() {
+    NativeModules.Recording.start(8000, 1024)
+    NativeAppEventEmitter.addListener('recording', this.recording.bind(this))
+  }
+
+  componentWillUnmount() {
+    NativeModules.Recording.stop()
+  }
+
+  recording(data) {
+    const power = Math.sqrt(data.reduce((result, value) => {
+      result += value * value
+      return result
+    }, 0)) / 4
+    Animated.timing(this.state.power, {
+      toValue: power,
+      duration: 100,
+    }).start()
+  }
+
+  handleLayout(event) {
+    this.setState({width: event.nativeEvent.layout.width})
   }
 
   render() {
-    return <View />
+    return <View style={{flex: 1}} onLayout={this.handleLayout.bind(this)}>
+      <Animated.View style={{
+        position: 'absolute',
+        bottom: 0,
+        width: this.state.width,
+        height: this.state.power,
+        backgroundColor: '#34495e',
+      }} />
+    </View>
   }
 }
