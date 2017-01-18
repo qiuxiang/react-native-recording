d0d7b2d5852803ffbbdbe0021334b0f8d4d71fe1
Qiu Xiang <i@7c00.cc>
2017-01-18 19:02:32 +0800

    use webview canvas to draw waveform

--- example/src/example.js
+++ example/src/example.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
 import {
   View,
-  Animated,
+  WebView,
   NativeModules,
   NativeAppEventEmitter } from 'react-native'
 
 export default class Main extends Component {
-  constructor() {
-    super()
-    this.state = {
-      power: new Animated.Value(0),
-      width: 0,
-    }
-  }
-
   componentDidMount() {
     NativeModules.Recording.start(8000, 1024)
     NativeAppEventEmitter.addListener('recording', this.recording.bind(this))
@@ -24,29 +16,13 @@ export default class Main extends Component {
   }
 
   recording(data) {
-    const power = Math.sqrt(data.reduce((result, value) => {
-      result += value * value
-      return result
-    }, 0)) / 4
-    Animated.timing(this.state.power, {
-      toValue: power,
-      duration: 100,
-    }).start()
-  }
-
-  handleLayout(event) {
-    this.setState({width: event.nativeEvent.layout.width})
+    this.webView.postMessage(data)
   }
 
   render() {
-    return <View style={{flex: 1}} onLayout={this.handleLayout.bind(this)}>
-      <Animated.View style={{
-        position: 'absolute',
-        bottom: 0,
-        width: this.state.width,
-        height: this.state.power,
-        backgroundColor: '#34495e',
-      }} />
-    </View>
+    return <WebView
+      ref={ref => this.webView = ref}
+      style={{flex: 1}}
+      source={require('./webview.html')} />
   }
 }
---
+++ example/src/webview.js
@@ -0,0 +1,33 @@
+var Waveform = function (selector) {
+  this.canvas = document.querySelector(selector)
+  this.canvas.width = document.body.clientWidth * 2
+  this.canvas.height = document.body.clientHeight * 2
+  this.context = this.canvas.getContext('2d')
+  this.context.strokeStyle = '#34495e'
+}
+
+Waveform.prototype.update = function (data) {
+  var slice = this.canvas.width / (data.length - 1)
+  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  this.context.beginPath()
+  data.reduce((function (x, value, index) {
+    var y = (2 + value / 256) * this.canvas.height / 4
+    if(index > 0) {
+      this.context.lineTo(x, y)
+    } else {
+      this.context.moveTo(x, y)
+    }
+    return x + slice
+  }).bind(this), 0)
+  this.context.stroke()
+}
+
+var waveform = new Waveform('#canvas')
+window.document.addEventListener('message', function (event) {
+  var data = event.data.split(',')
+  data.pop()
+  data = data.map(function (value) {
+    return parseFloat(value)
+  })
+  waveform.update(data)
+})
