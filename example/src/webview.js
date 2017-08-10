1fdc66f3a1060faaba409aee34ca79fc069487f4
7c00 <i@7c00.cc>
2017-08-11 01:05:32 +0800

    fix WebView bug

--- example/src/webview.js
+++ example/src/webview.js
@@ -1,17 +1,17 @@
-var Waveform = function (selector) {
+const Waveform = function (selector) {
   this.canvas = document.querySelector(selector)
-  this.canvas.width = document.body.clientWidth * 2
-  this.canvas.height = document.body.clientHeight * 2
+  this.canvas.width = document.body.clientWidth * window.devicePixelRatio
+  this.canvas.height = document.body.clientHeight * window.devicePixelRatio
   this.context = this.canvas.getContext('2d')
   this.context.strokeStyle = '#34495e'
 }
 
 Waveform.prototype.update = function (data) {
-  var slice = this.canvas.width / (data.length - 1)
+  const slice = this.canvas.width / (data.length - 1)
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
   this.context.beginPath()
   data.reduce((function (x, value, index) {
-    var y = (0.5 + value / 16384) * this.canvas.height
+    const y = (0.5 + value / 16384) * this.canvas.height
     if(index > 0) {
       this.context.lineTo(x, y)
     } else {
@@ -22,12 +22,15 @@ Waveform.prototype.update = function (data) {
   this.context.stroke()
 }
 
-var waveform = new Waveform('#canvas')
-window.document.addEventListener('message', function (event) {
-  waveform.update(event.data.split(',').map(function (value) {
-    return parseInt(value)
-  }))
-})
+// I don't know why android WebView can't get window width immediately
+setTimeout(() => {
+  const waveform = new Waveform('#canvas')
+  window.document.addEventListener('message', function (event) {
+    waveform.update(event.data.split(',').map(function (value) {
+      return parseInt(value)
+    }))
+  })
+}, 1)
 
 document.ontouchmove = function (event) {
   event.preventDefault()
